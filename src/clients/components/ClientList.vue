a56e31262d7191380c62afb86765059914ce7bb9
Kevin Rangel <krangel107@gmail.com>
2023-03-18 16:18:26 -0500

    Create Client List component

---
+++ src/clients/components/ClientList.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <ul>
+        <li>
+            <RouterLink :to="{
+                name: 'client-id',
+                params: { id: 1 }
+            }">
+                Client Name
+            </RouterLink>
+        </li>
+        <li>Client 2</li>
+    </ul>
+</template>
+
+
+
+<style scoped>
+
+</style>
--- src/clients/pages/ListPage.vue
+++ src/clients/pages/ListPage.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
+import ClientList from '@/clients/components/ClientList.vue';
+
         
 </script>
 <template>
     <div>
         <h1>Client List</h1>
+        <!-- Client List -->
+        <ClientList />
+
+
+        <!-- Pagination Number -->
+
+        <!-- LoadingModal -->
     </div>
 </template>
 
