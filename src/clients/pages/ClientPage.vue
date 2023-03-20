87b25c6755d512b59bba546626be02a8bf9df5ad
Kevin Rangel <krangel107@gmail.com>
2023-03-20 18:04:59 -0500

    add elements Cliente Page

--- src/clients/pages/ClientPage.vue
+++ src/clients/pages/ClientPage.vue
@@ -1,13 +1,36 @@
 <script setup lang="ts">
+import LoadingModal  from '@/shared/components/LoadingModal.vue';
+
         
 </script>
         
 <template>
-    <div>
-        <h1>Client by ID</h1>
-    </div>
+   <h3>Guardando...</h3>+
+   <h3>Guardado</h3>
+
+   <LoadingModal v-if="false"/>
+
+   <div>
+    <h1>Kevin Rangel</h1>
+    <form action="">
+        <input type="text" placeholder="Nombre del cliente" />
+        <input type="text" placeholder="Direccion" id="" />
+        <br>
+        <button type="submit">Guardar</button>
+    </form>
+   </div>
+   <code>
+    Toda la info del cliente
+   </code>
 </template>
 
 <style scoped>
-
+input {
+    width: 100%;
+    padding: 5px 10px;
+    margin-bottom: 10px;
+}
+button {
+    margin-bottom: 10px;
+}
 </style>
