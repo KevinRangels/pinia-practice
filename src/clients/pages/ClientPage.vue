aa74da3228782a3e97fdfa3426c421f3245a80d3
Kevin Rangel <krangel107@gmail.com>
2023-03-20 18:42:03 -0500

    Create function to updated client

--- src/clients/pages/ClientPage.vue
+++ src/clients/pages/ClientPage.vue
@@ -1,24 +1,34 @@
 <script setup lang="ts">
 import LoadingModal  from '@/shared/components/LoadingModal.vue';
+import { useMutation } from '@tanstack/vue-query';
 import { useRoute } from 'vue-router';
 import useClient from '../composables/useClient';
+import type { Client } from '../interfaces/client';
+import clientsApi from '../../api/clients-api';
 
 const route = useRoute()
 const { client, isLoading } = useClient(+route.params.id)
+
+const updatedClient = async(client: Client):Promise<Client> => {
+    const { data } = await clientsApi.patch<Client>(`/clients/${client.id}`, client)
+    return data
+}
+
+const clientMutation = useMutation( updatedClient )
         
 </script>
         
 <template>
-   <h3>Guardando...</h3>+
+   <h3>Guardando...</h3>
    <h3>Guardado</h3>
 
    <LoadingModal v-if="isLoading"/>
 
-   <div>
-    <h1>Kevin Rangel</h1>
-    <form action="">
-        <input type="text" placeholder="Nombre del cliente" />
-        <input type="text" placeholder="Direccion" id="" />
+   <div v-if="client">
+    <h1>{{ client.name}}</h1>
+    <form @submit.prevent="clientMutation.mutate(client!)">
+        <input type="text" placeholder="Nombre del cliente" v-model="client.name"/>
+        <input type="text" placeholder="Direccion" v-model="client.address"/>
         <br>
         <button type="submit">Guardar</button>
     </form>
